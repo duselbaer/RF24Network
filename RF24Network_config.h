792f39bf19d260704dffd4f7795f19a1455ff68c
Ronny Spiegel <ronny.spiegel@googlemail.com>
2014-11-04 18:46:02 +0000

    Temporarily kicked off all the other platforms to clean it up a bit

--- RF24Network_config.h
+++ RF24Network_config.h
@@ -1,4 +1,3 @@
-
 /*
  Copyright (C) 2011 James Coliz, Jr. <maniacbug@ymail.com>
 
@@ -7,61 +6,21 @@
  version 2 as published by the Free Software Foundation.
  */
 
-#ifndef __RF24_CONFIG_H__
-#define __RF24_CONFIG_H__
+#pragma once
 
-#ifdef __MSP430__
-/* Put all the MSP430 specific stuff in here */
-#define PSTR
-#define IF_SERIAL_DEBUG(x)
+#if defined __MSP430__
+
+#elif defined ARDUINO
+
 #else
-#if ARDUINO < 100
-#include <WProgram.h>
-#else
-#include <Arduino.h>
+# define IF_SERIAL_DEBUG(x)
+# define snprintf_P snprintf
+  typedef char const prog_char;
+  typedef uint16_t prog_uint16_t;
+# define PSTR(x) (x)
+# define printf_P printf
+# define strlen_P strlen
+# define PROGMEM
+# define pgm_read_word(p) (*(p)) 
+# define PRIPSTR "%s"
 #endif
-
-#include <stddef.h>
-
-// Stuff that is normally provided by Arduino
-#ifndef ARDUINO
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-extern HardwareSPI SPI;
-#define _BV(x) (1<<(x))
-#endif
-
-#undef SERIAL_DEBUG
-#ifdef SERIAL_DEBUG
-#define IF_SERIAL_DEBUG(x) ({x;})
-#else
-#define IF_SERIAL_DEBUG(x)
-#endif
-
-// Avoid spurious warnings
-#if ! defined( NATIVE ) && defined( ARDUINO )
-#undef PROGMEM
-#define PROGMEM __attribute__(( section(".progmem.data") ))
-#undef PSTR
-#define PSTR(s) (__extension__({static const char __c[] PROGMEM = (s); &__c[0];}))
-#endif
-
-// Progmem is Arduino-specific
-#ifdef ARDUINO
-#include <avr/pgmspace.h>
-#define PRIPSTR "%S"
-#else
-typedef char const prog_char;
-typedef uint16_t prog_uint16_t;
-#define PSTR(x) (x)
-#define printf_P printf
-#define strlen_P strlen
-#define PROGMEM
-#define pgm_read_word(p) (*(p)) 
-#define PRIPSTR "%s"
-#endif
-#endif // MSP430
-
-#endif // __RF24_CONFIG_H__
-// vim:ai:cin:sts=2 sw=2 ft=cpp
